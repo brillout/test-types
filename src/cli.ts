f8d360290a654e6368fd13f13f33b4d2c3ee3bf9
Romuald Brillout <git@brillout.com>
2022-09-26 12:56:36 +0200

    minor refactor

--- src/cli.ts
+++ src/cli.ts
@@ -1,41 +1,14 @@
 // @ts-ignore
 import 'source-map-support/register'
 import { testTypes } from './testTypes'
-import type { FindFilter } from './utils'
+import { findFilesParseCliArgs } from './utils'
 
 initPromiseRejectionHandler()
 cli()
 
 function cli() {
-  const { filter, debug } = parseArgs()
+  const { filter } = findFilesParseCliArgs()
   testTypes(filter)
-}
-
-function parseArgs(): { filter: null | FindFilter; debug: boolean } {
-  let debug = false
-  const terms: string[] = []
-  let exclude = false
-  process.argv.slice(2).forEach((arg) => {
-    if (arg === '--debug') {
-      debug = true
-    } else if (arg === '--exclude') {
-      exclude = true
-    } else {
-      terms.push(arg)
-    }
-  })
-
-  const filter =
-    terms.length === 0
-      ? null
-      : {
-          terms,
-          exclude
-        }
-  return {
-    filter,
-    debug
-  }
 }
 
 function initPromiseRejectionHandler() {
