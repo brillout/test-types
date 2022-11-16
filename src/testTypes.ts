5f4e756377a420f32ae00e5ee6510e40701ee24e
Romuald Brillout <git@brillout.com>
2022-11-16 16:04:01 +0100

    feat: implement config `.testTypes.json`

--- src/testTypes.ts
+++ src/testTypes.ts
@@ -3,15 +3,19 @@ export { testTypes }
 import { runCommand, FindFilter, logProgress } from './utils'
 import { findTypescriptCode } from './findTypescriptCode'
 import { isTTY } from './utils/isTTY'
+import assert from 'assert'
 
 async function testTypes(filter: null | FindFilter) {
   const typescriptCode = await findTypescriptCode(filter)
 
   for (const tsCode of typescriptCode) {
     const { tsProjectRootDir } = tsCode
-    const logMsg = `[TypeScript Check] ${tsCode.tsConfigFilePath ?? tsCode.tsFilePath}`
+    const cmd =
+      loadTestTypescriptConfig(tsProjectRootDir)?.testCommand ??
+      `npx tsc --noEmit --skipLibCheck --esModuleInterop ${tsCode.tsFilePath ?? ''}`.trim()
+    const logMsg = `[TypeScript Check] ${tsCode.tsConfigFilePath ?? tsCode.tsFilePath} (${cmd})`
     const done = logProgress(logMsg)
-    await runCommand(`npx tsc --noEmit --skipLibCheck --esModuleInterop ${tsCode.tsFilePath ?? ''}`, {
+    await runCommand(cmd, {
       cwd: tsProjectRootDir,
       timeout: 120 * 1000,
     })
@@ -20,4 +24,20 @@ async function testTypes(filter: null | FindFilter) {
       console.log(logMsg)
     }
   }
+}
+
+function loadTestTypescriptConfig(tsProjectRootDir: string): null | { testCommand: string } {
+  let config: Record<string, unknown>
+  try {
+    config = require(`${tsProjectRootDir}/.testTypes.json`)
+  } catch (err) {
+    if ((err as any).code === 'MODULE_NOT_FOUND') {
+      return null
+    }
+    throw err
+  }
+  assert(Object.keys(config).length === 1)
+  const { testCommand } = config
+  assert(typeof testCommand === 'string')
+  return { testCommand }
 }
